a71880f96afa8b1b2fb948d90c4158eb2dc46673
Gonzalo Orsi <gonzaloorsi@gmail.com>
2024-01-26 19:45:50 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [selected, setSelected] = React.useState('two');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form>
+        <fieldset>
+        <legend>Select one value</legend>
+        <select value={selected} onChange={(event) => setSelected(event.target.value)}>
+          <option value="one">One</option>
+          <option value="two">Two</option>
+          <option value="three">Three</option>
+          <option value="four">Four</option>
+          <option value="five">Five</option>
+        </select>
+        </fieldset>
+      </form>
+      
+      <p>Selected value: {selected}</p>
     </div>
   );
 }
